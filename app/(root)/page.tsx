715cd2278aa22a0eb89f35945b68a5a76b9a7416
alex <alexander-eu@hotmail.com>
2025-01-15 21:57:41 +0200

    Added products mocked data and general products listing on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 export const metadata = {
   title: 'Home',
 };
 
 const Homepage = () => {
   return <div>
-    Prostore
+    <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
   </div>;
 };
 
