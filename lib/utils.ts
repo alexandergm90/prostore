7b03727bd9679908a4d9d3384072e6e9ff530703
alex <alexander-eu@hotmail.com>
2025-01-20 21:15:15 +0200

    Added product prisma model and db data fetch

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Convert a prisma object into a regular JS object
+export function convertToPlainObject(obj: any) {
+  return JSON.parse(JSON.stringify(obj))
+}
