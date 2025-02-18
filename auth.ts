7409a2dcc0ab974c13b0d5d6b3b8d339306d021e
alex <alexander-eu@hotmail.com>
2025-02-18 20:45:17 +0200

    Add JWT token customization

--- auth.ts
+++ auth.ts
@@ -48,16 +48,30 @@ export const config = {
         })
     ],
     callbacks: {
-        async jwt({ token, user }) {
+        async jwt({ token, user, trigger, session }: any) {
             if (user) {
-                token.id = user.id;
-                token.email = user.email;
+                token.role = user.role;
+
+                if(user.name === 'NO_NAME') {
+                    token.name = user.email!.split('@')[0]
+
+                    await prisma.user.update({
+                        where: {
+                            id: user.id
+                        },
+                        data: {
+                            name: token.name
+                        }
+                    })
+                }
             }
             return token;
         },
         async session({session, user, trigger, token}: any){
             // Set the user ID from the token
             session.user.id = token.sub;
+            session.user.role = token.role;
+            session.name = token.name;
 
             // If there is an update, set the user name
             if(trigger === 'update') session.user.name = user.name
