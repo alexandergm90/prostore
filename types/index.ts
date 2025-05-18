7c86c28cc3836daacd39dadce99a29b5a68d3b32
alex <alexander-eu@hotmail.com>
2025-05-18 16:12:26 +0300

    Zod schemas & types for order & order items

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,14 @@
-import { z } from 'zod';
-import {insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema} from "@/lib/validator";
+import {z} from 'zod';
+import {
+    insertProductSchema,
+    insertCartSchema,
+    cartItemSchema,
+    shippingAddressSchema,
+    insertOrderSchema,
+    insertOrderItemSchema
+} from "@/lib/validator";
 
-export type Product = z.infer<typeof  insertProductSchema> & {
+export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
     rating: string;
     createdAt: Date;
@@ -10,3 +17,14 @@ export type Product = z.infer<typeof  insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string;
+    createdAt: Date;
+    isPaid: boolean;
+    paidAt: Date | null;
+    isDelivered: boolean;
+    deliveredAt: Date | null;
+    orderitems: OrderItem[];
+    user: { name: string; email: string }
+};
